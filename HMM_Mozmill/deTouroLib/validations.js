3c8b5811fd9562c937f12a6666a109ab7c58cb7f
Vlad Maniac <maniacvld@gmail.com>
2013-01-30 11:14:54 +0200

    Form validation helper method. Part of Validation Type

--- HMM_Mozmill/deTouroLib/validations.js
+++ HMM_Mozmill/deTouroLib/validations.js
@@ -1,9 +1,3 @@
-// License
-
-// Lib requirements
-
-// Constants
-
 /**
  * Constructor
  */
@@ -27,5 +21,18 @@ Validation.prototype = {
    */
   get controller() {
     return this._controller;
+  },
+
+  /**
+   * Data validation of form fields, input fields, text fields
+   *
+   * @returns Boolean
+   */
+  checkForm: function Validation_checkForm(aFormId, aContent) {
+    var formElement = new elementslib.ID(this.controller.window.document, aFormId);
+
+    this.controller.assert(function () {
+      return formElement.getNode().value === aContent;
+    }, "Form content matches expectation: " + aContent);
   }
 }
