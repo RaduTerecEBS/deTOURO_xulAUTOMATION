aa1378e2252ebcc636a37b62c259cf24845b8814
Vlad Maniac <maniacvld@gmail.com>
2013-02-06 17:48:21 +0200

    A bit of scripting in test2.js - KT part

--- HMM_Mozmill/tests/detouro/testLogin/test2.js
+++ HMM_Mozmill/tests/detouro/testLogin/test2.js
@@ -17,5 +17,31 @@ function teardownModule() {
 
 // Verify login successful and user is seen in KT also
 function testLoginKT() {
+  var kt,
+    laufende;
 
+  // open ebs.hmm.lan
+  controller.open(PAGE_SOURCE);
+  controller.waitForPageLoad();
+
+  // get the list element to enter detouro app and check
+  kt =  new elementslib.XPath(controller.tabs.activeTab, "/html/body/div[@id='content-outer']/" +
+                                                         "div[@id='content']/div/div[@id='banners']/" +
+													     "table[2]/tbody/tr[1]/td[5]/a/b");
+  controller.click(kt);
+  controller.waitForPageLoad();
+
+  //XXX: sleep for testing purposes
+  controller.sleep(2000);
+
+  // Click on "Laufende"
+  laufende = new elementslib.ID(controller.window.document, 
+  	                            "ctl00_MainContent_ASPxMenuDisplayAuctions_DXI2_T");
+
+  controller.waitForPageLoad();
+
+  //XXX: sleep for testing purposes
+  controller.sleep(2000);
+
+  
 }
