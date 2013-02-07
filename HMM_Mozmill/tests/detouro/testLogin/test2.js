c726dd9b4f5253cb454ce34646b10519eeab6f7a
Vlad Maniac <maniacvld@gmail.com>
2013-02-07 10:13:45 +0200

    A little bit of housekeeping...

--- HMM_Mozmill/tests/detouro/testLogin/test2.js
+++ HMM_Mozmill/tests/detouro/testLogin/test2.js
@@ -18,6 +18,7 @@ function teardownModule() {
 // Verify login successful and user is seen in KT also
 function testLoginKT() {
   var kt,
+    ktas,
     laufende;
 
   // open ebs.hmm.lan
@@ -27,21 +28,28 @@ function testLoginKT() {
   // get the list element to enter detouro app and check
   kt =  new elementslib.XPath(controller.tabs.activeTab, "/html/body/div[@id='content-outer']/" +
                                                          "div[@id='content']/div/div[@id='banners']/" +
-													     "table[2]/tbody/tr[1]/td[5]/a/b");
+													                               "table[2]/tbody/tr[1]/td[5]/a/b");
   controller.click(kt);
   controller.waitForPageLoad();
 
   //XXX: sleep for testing purposes
   controller.sleep(2000);
 
+  // XXX: Bitte nicht XPATH verwenden, nur wenn gibt es nicht etwas anderes
+  ktas = new elementslib.XPath(controller.tabs.activeTab, "/html/body/form[@id='aspnetForm']/" +
+                                                          "div[3]/div[2]/div[2]/div/div/div/" +
+															                            "div/span");
+  
+  controller.click(ktas);
+  controller.waitForPageLoad();
+
   // Click on "Laufende"
-  laufende = new elementslib.ID(controller.window.document, 
+  laufende = new elementslib.ID(controller.tabs.activeTab, 
   	                            "ctl00_MainContent_ASPxMenuDisplayAuctions_DXI2_T");
 
+  controller.click(laufende);
   controller.waitForPageLoad();
 
   //XXX: sleep for testing purposes
   controller.sleep(2000);
-
-  
 }
