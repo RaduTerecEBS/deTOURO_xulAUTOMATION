7e7f6e5e232fdb7af20f343481d65cea61225e85
Vlad Maniac <maniacvld@gmail.com>
2013-02-28 10:26:52 +0200

    Fix testSearch/testSearchByNumber.js as due to population of the grid view with lots of data

--- HMM_Mozmill/tests/detouro/testSearchKt/testSearchByNumber.js
+++ HMM_Mozmill/tests/detouro/testSearchKt/testSearchByNumber.js
@@ -29,12 +29,16 @@ function testSearchByNumber() {
   	                           "ctl00_MainContent_ASPxGridViewDrives_DXFREditorcol1_I");
 
   controller.type(noField, TEST_DATA);
+  controller.waitFor(function () {
+    return noField.getNode().value === TEST_DATA;
+  }, "Test data typed correctly");
+
   controller.waitForPageLoad();
   controller.sleep(7000);
   
   var table = controller.tabs.activeTab.querySelectorAll(".dxgvDataRow_deTouroKT");
 
-  for (var i = 0, l = table.length; i < l - 2; i++) {
+  for (var i = 0, l = table.length; i < l - 20; i++) {
     controller.assert(function () {
       return table[i].textContent.contains(TEST_DATA);
     },"Suggestion contains the search term");
