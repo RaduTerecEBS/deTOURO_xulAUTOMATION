d1df5fbe26ac1cb5c9327bd61096bc108b4828dd
Vlad Maniac <maniacvld@gmail.com>
2013-04-01 14:52:37 +0300

    Issue 17 - testSearchByDestinationPlace.js fix

--- HMM_Mozmill/tests/detouro/testSearchKt/testSearchByDestinationPlace.js
+++ HMM_Mozmill/tests/detouro/testSearchKt/testSearchByDestinationPlace.js
@@ -24,11 +24,11 @@ function testSearchByDestinationPlace() {
     return enter;
   }, "Success in entering deTouro KT");
 
-  kt.search("ctl00_MainContent_ASPxGridViewDrives_DXFREditorcol7_I", TEST_DATA);
+  kt.search("ctl00_MainContent_ASPxGridViewDrives_DXFREditorcol6_I", TEST_DATA);
 
   //XXX: if first row cell is present then we have at least one result for the given test data
   testResultAtLeastOne = new elementslib.ID(controller.window.document,
-                                            "ctl00_MainContent_ASPxGridViewDrives_tccell0_7");
+                                            "ctl00_MainContent_ASPxGridViewDrives_tccell0_6");
   // XXX: used containes until the spaces bug are eliminated
   controller.waitFor(function() {
     return testResultAtLeastOne.getNode().textContent.toLowerCase().contains(TEST_DATA);
